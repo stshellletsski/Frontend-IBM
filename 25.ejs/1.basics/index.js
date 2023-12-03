aad5b777f66197480921d981b202105b4adfea6e
Marcin Strzelecki <118393661+stshellletsski@users.noreply.github.com>
2023-12-03 01:51:08 +0000

    update EJS

--- 25.ejs/1.basics/index.js
+++ 25.ejs/1.basics/index.js
@@ -4,13 +4,13 @@ const app = express();
 const port = 3000;
 const array1 = ["weekday", "weekend"];
 const array2 = ["work hard", "have fun"];
-const array3 = ["The Best", "The Most Amazing", "The Most Magnificent", "The Most Incredible"]
+const array3 = ["The Best", "The Most Amazing", "The Most Magnificent", "The Most Incredible"];
 
 function day2Data (day) {
     if(day < 6 && day > 0) {
-    return 0
+    return 0;
     } else {
-    return 1
+    return 1;
     }
 }
 
